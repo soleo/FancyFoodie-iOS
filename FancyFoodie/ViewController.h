f2779579c4a6e377aac9b9717151fc016b6d0c24
Xinjiang Shao <shaoxinjiang@gmail.com>
2015-09-23 20:23:48 -0500

    Start an empty project

---
+++ FancyFoodie/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FancyFoodie
+//
+//  Created by Xinjiang Shao on 9/23/15.
+//  Copyright © 2015 Xinjiang Shao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
